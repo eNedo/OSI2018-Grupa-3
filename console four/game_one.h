b80f2515a20a8ef6ea97ad2befdf025cc2e088bb
Grujica Vasic <grujica.vasic@hotmail.com>
2019-01-03 17:24:43 +0100

    v1

--- console four/game_one.h
+++ console four/game_one.h
@@ -1,3 +1,6 @@
 #pragma once
+#include <stdio.h>
+#include <time.h>
+#include <Windows.h>
 
 void prvaIgra(int brojIgranja, int pobijede, int gubici);		// brojIgranja - ukupan broj pokretanja PRVE IGRE od strane korisnika; broj pobijeda, broj gubitaka
