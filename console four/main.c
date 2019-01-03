49512a7201b0bdfef473edcce9d55abfbd57f940
eNedo <32650507+eNedo@users.noreply.github.com>
2019-01-03 20:27:33 +0100

    Update main.c

--- console four/main.c
+++ console four/main.c
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <conio.h>
 #include "game_one.h"
 #include "game_two.h"
@@ -12,9 +13,21 @@
 #include "keys.h"
 int main()
 {
-	fullscreen(); 
- 	LogIn(); 
-
- 	system("pause"); 
-	return 0; 
+	fullscreen();
+	LogIn();
+	
+ 
+	system("pause");
+	return 0;
 }
+
+
+
+
+
+/*
+struct tm tm;
+localtime_s(&tm, &(time_t) { time(NULL) });
+char *time = malloc(sizeof(char) * 26);
+asctime_s(time, 26, &tm);
+*/
