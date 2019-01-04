778adcc118c4e48b4c7b2d8c080cca114c5c670d
eNedo <32650507+eNedo@users.noreply.github.com>
2019-01-04 15:40:50 +0100

    v3

--- console four/stats.h
+++ console four/stats.h
@@ -8,20 +8,20 @@
 #include <ctype.h>
 void StatsUpdate(int game_id, char *time_played, int points, int number_of_wins)
 {
-	FILE *stats; fopen_s(&stats, "statistics.txt", "r");
+	FILE *stats; fopen_s(&stats, "statistics.txt", "r+");
 	if (stats) {
 		int gameidx, pointsx, numberofwinsx;
 		char *timex = malloc(26); 
+		rewind(stats);
 		for (int i = 0; i < 10; i++)
 		{
-			fscanf_s(stats, "%d %d %d %s", &gameidx, &pointsx, &numberofwinsx,timex); 
+			fscanf_s(stats, "%d %d %d %s", &gameidx, &pointsx, &numberofwinsx,timex,26); 
+			printf("%d %d %d %s", gameidx, pointsx, numberofwinsx, timex); 
 					if (number_of_wins>numberofwinsx)		// writing in order  
 					{	  
-						fclose(stats); fopen_s(&stats, "statistics.txt", "w"); 
-						fprintf_s(stats, "%d %d %d %s \n", game_id, points, number_of_wins, time_played);  
-						fclose(stats); fopen_s(&stats, "statistics.txt", "r");
-						break; // needs more work 
-					}		
+						fprintf_s(stats, "%d%d%d%s", game_id, points, number_of_wins, time_played);  
+						break;  // needs more work
+					}			// rewriting line ^
 		
 		fclose(stats);			// end of work with stats file 
 		}
@@ -34,27 +34,31 @@ void StatsUpdate(int game_id, char *time_played, int points, int number_of_wins)
 }
 void ShowStatistics()
 {
-	FILE *stats; fopen_s(&stats, "statistics.txt", "r");
+	FILE *statsf; fopen_s(&statsf, "statistics.txt", "r");
 	int game_id; // 1-first game 2-second.. 4.. 
 	int points,num_of_wins; 
 	char *date=malloc(26); 
-	if (stats)
+	if (statsf)
 	{
 		cls();
 		MoveCursorToMid();
-		printf("Igra          Broj poena     Datum igranja     Broj pobjeda  ");
+		printf("    Igra                 Broj poena         Broj pobjeda     Datum igranja   ");
 		MoveCursorNextRow();
+
 		for (int i = 0; i < 10; i++)
 		{
-			fscanf_s(stats, "%d%d%d%s", &game_id, &points, &num_of_wins, date);
+			fscanf_s(statsf, "%d%d%d%s", &game_id, &points, &num_of_wins, date,26);
 			if (game_id == 1) printf("%d)Pogadjanje broja ", i + 1);
 			else if (game_id == 2) printf("%d)Kviz             ", i + 1);
 			else if (game_id == 3) printf("%d)Loto             ", i + 1);
 			else if (game_id == 4) printf("%d)IX-OX            ", i + 1);
-			printf("%d           %s                     %d", points, date, num_of_wins);
+			printf("         %d                 %d              %s \n", points, num_of_wins,date);
 			MoveCursorNextRow();
 		}
-		fclose(stats); //need more work + after 10th line delete all records
+ 		fclose(statsf); 
+		fopen_s(&statsf, "statistics.txt", "r+");  fseek(statsf, 460, SEEK_CUR);
+		for (int i = 0; i < 15; i++) fprintf(statsf, "                                           \n");   
+		fclose(statsf);				// remove more than 10. entries 
 	}
 	else
 	{
