706b1709902157284b597b9e1708b85e6d8d756f
eNedo <32650507+eNedo@users.noreply.github.com>
2019-01-08 18:53:55 +0100

    v1.2
    -switched to safe functions 
    -added else statements if file openings fail

--- console four/csv.h
+++ console four/csv.h
@@ -1,28 +1,36 @@
-#pragma once
+#include <Windows.h>
+#include <stdlib.h>
 #include <stdio.h>
-
-void inCSV();	//takes three integers and string from statistics.txt, then write them in inCSV.csv (takes data row by row)
-
-void inCSV()
+#include <time.h>
+#include <conio.h>
+ 
+//takes three integers and string from statistics.txt, then write them in inCSV.csv
+//(takes data row by row)
+void  CSV()
 {
 	int gameID, points, numberOfWins;
 	char* string[101];
 	FILE* fp, *fp1;
-	if ((fp = fopen("statistics.txt", "r")) != NULL)		//opens textual file
+	fopen_s(&fp, "statistics.txt", "r");
+	fopen_s(&fp1, "inCSV.csv", "w");
+	if (fp!=NULL)		//opens textual file
 	{
-		if ((fp1 = fopen("inCSV.csv", "w")) != NULL)		//opens CSV file
+		if (fp1!= NULL)		//opens CSV file
 		{
 			char s[101];
-			fscanf(fp, "%d %d %d %s", &gameID, &points, &numberOfWins, string);
-			fprintf(fp1, "%d,%d,%d,%s\n", gameID, points, numberOfWins, string);
-
+			fscanf_s(fp, "%d %d %d %s", &gameID, &points, &numberOfWins, string,30);
+			fprintf_s(fp1, "%d,%d,%d,%s\n", gameID, points, numberOfWins, string,30);
 			while (fgets(s, 101, fp) != NULL)
 			{
-				fscanf(fp, "%d %d %d %s", &gameID, &points, &numberOfWins, string);
-				fprintf(fp1, "%d,%d,%d,%s\n", gameID, points, numberOfWins, string);
+				fscanf_s(fp, "%d %d %d %s", &gameID, &points, &numberOfWins, string,30);
+				fprintf_s(fp1, "%d,%d,%d,%s\n", gameID, points, numberOfWins, string,30);
 			}
 			fclose(fp1);
 			fclose(fp);
 		}
+		else
+			printf("error!");
 	}
+	else
+		printf("critical error!"); 
 }
