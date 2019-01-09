ef431ee51314d91dee2e536147dd2168c97f801b
Dejan Tesinic <45770466+Dejant2504@users.noreply.github.com>
2019-01-09 20:19:03 +0100

    Update game_two.h

--- console four/game_two.h
+++ console four/game_two.h
@@ -1,92 +1,86 @@
 #pragma once
-#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-StatsUpdate(int game_id, char *time_played, int points, int number_of_wins);
-int main()
+#include <ctype.h>
+
+void Game_two()
 {
     FILE *fp;
-    char *pitanje, *odg_a, *odg_b, *odg_c, tac_odg, unos_odg;
-    int duz_pit = 0, duz_a = 0, duz_b = 0, duz_c = 0, random_number; // random broj
-    if ( (fp = fopen("Test.txt","r")) != NULL)
+    int points = 0;
+    if ( (fp = fopen("Test.txt", "r")) != NULL)
     {
-        char c;
-        int i = 0, j = 0, k = 0, p = 0, q = 0, m = 0, redni_br = 0, pocetna_alokacija = 0; // dodatak - broj bajtova do pocetka pitanja.
-        random_number = rand() % 5;
-        printf("random_number = %d   ", random_number);
-        while ((c = fgetc(fp)) != EOF  && redni_br < 1)
+        char pitanje[35], odg1[15], odg2[15], odg3[15], c;
+        int  unos_odg, tac_odg, i;
+        int  random_number, redni_br_pitanja = 0,  svi_odg_tacni = 0, pocetni_pomjeraj = 0, niz[5] ;
+        for(i = 0; i < 5; i++)
         {
-            if (c == '!')
-            redni_br++;
-            printf("%c %d\n", c, pocetna_alokacija);
-            pocetna_alokacija++;
+            int j = 0, redni_br_pitanja = 0;
+            if ( i == 0)
+            {
+            random_number = rand() % 5;
+            niz[0] = 0;
+            }
+            else
+            {
+                while ( j < i )
+                {
+                    if ( niz[j] == random_number)
+                    {
+                        random_number = rand() % 5;
+                        j = 0;
+                    }
+                    j++;
+                }
+            }
+
+        niz[i] = random_number;
+        int pocetni_pomjeraj = 0;
+        while ( (c = fgetc(fp)) != EOF && redni_br_pitanja < random_number )
+        {
+                    if ( c == '!')
+                        redni_br_pitanja++;
+                    pocetni_pomjeraj++;
         }
-        printf("pocetna_alokacija = %d\n", pocetna_alokacija);
-        fseek(fp, pocetna_alokacija + 5, SEEK_SET); //
-        while ( (c = fgetc(fp)) && c != ':'  ) // 
-            duz_pit++;
-        printf("\nduz_pit = %d\n", duz_pit);// premotavanje na pocetak
-        pitanje = (char*)calloc(duz_pit + 1,sizeof(char)); // alociranje pitanja
-        fseek(fp, pocetna_alokacija + 5, SEEK_SET);
-        while ((c = fgetc(fp))  &&  c != ':') //
+        fseek(fp, pocetni_pomjeraj + redni_br_pitanja * 6, SEEK_SET);
+        fscanf(fp, "%s", pitanje);
+        fseek(fp, strlen(pitanje) + pocetni_pomjeraj + redni_br_pitanja * 6, SEEK_SET);
+        fscanf(fp, "%s", odg1);
+        fscanf(fp, "%s", odg2);
+        fscanf(fp, "%s", odg3);
+        fscanf(fp, "%d", &tac_odg);
+        printf("%d. %s\n", i + 1,  pitanje);
+        printf("1) %s\n", odg1);
+        printf("2) %s\n", odg2);
+        printf("3) %s\n", odg3);
+        do
         {
-            printf("%c", c);
-            pitanje[i] = c;
-            i++;
+        printf("Unesi odgovor 1,  2, ili 3: ");
+        scanf("%d", &unos_odg);
+        } while ( unos_odg < 1 || unos_odg > 3);
+        if ( unos_odg == tac_odg)
+        {
+            printf("TACAN ODGOVOR!\n");
+            printf("DOBILI STE 20 BODOVA!\n\n");
+            points += 20;
+            svi_odg_tacni++;
+        }
+        else
+        {
+            printf("NETACAN ODGOVOR!\n");
+            printf("IZGUBILI STE 30 BODOVA!\n\n");
+            points -= 30;
+        }
+        if (svi_odg_tacni == 4)
+        {
+            printf("CESTITAM! DOBILI STE BONUS 50 BODOVA ZA SVE TACNE ODGOVORE!\n\n");
+            points += 50;
+        }
+        redni_br_pitanja = 0, pocetni_pomjeraj = 0;
         }
         printf("\n");
-        fseek(fp, duz_pit + pocetna_alokacija, SEEK_SET); // +1  premotavanje na mjesto gdje pocinje odg_a
-        while ((c = fgetc(fp)) != EOF && c != ':')
-        {
-            duz_a++;
-        }
-        odg_a = (char*)calloc(duz_a, sizeof(char)); // + 1
-        fseek(fp,duz_pit+ pocetna_alokacija, SEEK_SET); // + 1
-        while ((c = fgetc(fp)) != EOF && c != ':')
-        {
-            printf("%c", c);
-            odg_a[j] = c;
-            j++;
-        }
-        fseek(fp, duz_pit + duz_a  + pocetna_alokacija, SEEK_SET); // + 1
-        while ((c = fgetc(fp)) != EOF && c != ':')
-        {
-            duz_b++;
-        }
-        fseek(fp, duz_pit + duz_a  + pocetna_alokacija, SEEK_SET); // + 3
-        odg_b = (char*)calloc(duz_b, sizeof(char));
-        while ((c = fgetc(fp)) != EOF && c != ':')
-        {
-            odg_b[k] = c;
-            k++;
-        }
-        fseek(fp, duz_pit + duz_a + duz_b + 1 + pocetna_alokacija, SEEK_SET);
-        while ((c = fgetc(fp)) != EOF && c != ':')
-            duz_c++;
-        fseek(fp, duz_pit + duz_a + duz_b + 14 + pocetna_alokacija, SEEK_SET);
-        odg_c = (char *)calloc(duz_c, sizeof(char));
-        while ((c = fgetc(fp)) != EOF && c != ':')
-        {
-            printf(" %c ", c);
-            odg_c[p] = c;
-            p++;
-        }
-        fseek(fp, duz_pit + duz_a + duz_b + duz_c + 16 + pocetna_alokacija, SEEK_SET);
-        while ((c = fgetc(fp)) != EOF && c != ':')
-            tac_odg = c;
-        printf("%s? \n", pitanje);
-        printf("a) %s \n", odg_a);
-        printf("b) %s \n", odg_b);
-        printf("c) %s \n", odg_c);
-        printf("tac odg %c\n", tac_odg);
-        printf("UNESITE ODGOVOR (a, b, ili c): ");
-        scanf("%c", &unos_odg);
-        if ( unos_odg == tac_odg)
-            printf("TACAN ODGOVOR!");
-        else
-            printf("NETACAN ODGOVOR!");
+        fclose(fp);
     }
-    return 0;
+    printf("\nUKUPAN BROJ BODOVA NA KRAJU OVE IGRE JE: %d \n", points);
 }
 
