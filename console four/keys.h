506e0346f651bc2e278a21320264590a76237b34
eNedo <32650507+eNedo@users.noreply.github.com>
2019-01-10 15:38:36 +0100

    Update keys.h

--- console four/keys.h
+++ console four/keys.h
@@ -3,20 +3,15 @@
 #include <string.h>
 #include <time.h>
 #include <conio.h>
-void format_time(char *output) {
-	time_t rawtime;
-	struct tm * timeinfo;
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-	sprintf(output, "%d-%d-%d-%d:%d:%d", timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-}
-time_t date_string_to_time(const char *date) {
+#include "utility.h"
+#include "stats.h"
+ time_t date_string_to_time( char *date) {
 	struct tm tm = { 0 }; // Important, initialize all members
-	sscanf(date, "%d-%d-%d-%d:%d:%d", &tm.tm_mday, &tm.tm_mon, &tm.tm_year,
+	sscanf_s(date, "%d-%d-%d-%d:%d:%d", &tm.tm_mday, &tm.tm_mon, &tm.tm_year,
 		&tm.tm_hour, &tm.tm_min, &tm.tm_sec);
 	tm.tm_isdst = -1; // Assume local daylight setting per date/time
 	tm.tm_mon--;      // Months since January
-	// Assume 2 digit year if in the range 2000-2099, else assume year as given
+					  // Assume 2 digit year if in the range 2000-2099, else assume year as given
 	if (tm.tm_year >= 0 && tm.tm_year < 100) {
 		tm.tm_year += 2000;
 	}
@@ -24,31 +19,30 @@ time_t date_string_to_time(const char *date) {
 	time_t t = mktime(&tm);
 	return t;
 }
-void WriteKeyTimer(int gamenumber, int duration)//upisuje u drugi txt fajl broj igrice na koju se ključ odnosi, njegov datum upisivanja i trajanje ključa u danima
+void WriteKeyTimer(int gamenumber, int duration) 
 {
 	FILE *dat, *temp;
 	int tmpgn, tmpdur;
+	char *time123 = (char*)malloc(26);
 	if (dat = fopen("keys_timer.txt", "r"))
 	{
-		char time[30];
-		rewind(dat);
 		if (temp = fopen("temp.txt", "w"))
 		{
-			while (fscanf(dat, "%d %s %d", &tmpgn, time, &tmpdur))
+			while (fscanf_s(dat, "%d %s %d", &tmpgn, time123,26, &tmpdur)!=EOF)
 			{
-				fprintf(temp, "%d %s %d\n", tmpgn, time, tmpdur);
+				fprintf_s(temp, "%d %s %d\n", tmpgn, time123,26, tmpdur);
 			}
-			format_time(time);
-			fprintf(temp, "%d %s %d\n", gamenumber, time,duration);
+			char *a = "-"; char *b = " ";
+			char *c = string_replace(Time(), b, a); 
+			fprintf_s(temp, "%d %s %d ", gamenumber, string_replace(c,"\n", " "),duration);
 		}
-		remove("keys_timer.txt");
-		rename("temp.txt", "keys_timer.txt");
-		rewind(temp);
 		fclose(temp);
 		fclose(dat);
+		remove("keys_timer.txt");
+		rename("temp.txt", "keys_timer.txt");
 	}
 }
-int CheckKeyFormat(char* key)//provjerava format ključa i vraća 0 ili 1 u zavisnosti
+int CheckKeyFormat(char* key) 
 {
 	if (strlen(key) != 19)
 		return 0;
@@ -65,20 +59,20 @@ int CheckKeyFormat(char* key)//provjerava format ključa i vraća 0 ili 1 u zavi
 	}
 	return 1;
 }
-int CheckKeyValidity(char* key, int gamenumber)//provjerava validnost ključa a ako je ispravan poziva funckiju WriteKeyTimer(int,int)
+int CheckKeyValidity(char* key, int gamenumber) 
 {
 	FILE *dat;
 	if ((dat = fopen("keys.txt", "r")) != NULL)
 	{
 		char filekey[20];
 		int nmb, duration, active, br = 0;
-		while (fscanf(dat, "%d %d ,%s %d", &active, &nmb, filekey, &duration) != EOF)
+		while (fscanf(dat, "%d %d %s %d", &active, &nmb, filekey, &duration) != EOF)
 		{
 			br++;
-			if (!strcmp(key, filekey) && nmb == gamenumber)
+			if ((strcmp(key, filekey)==0) && nmb == gamenumber)
 			{
 				if (active == 1)
-					return -1;//ako je ključ već prije aktiviran onda je nevažeći i funckija vraća -1 u tom slučaju
+					return -1; 
 				else
 				{
 					WriteKeyTimer(gamenumber, duration);
@@ -87,57 +81,51 @@ int CheckKeyValidity(char* key, int gamenumber)//provjerava validnost ključa a 
 					FILE *temp;
 					if (temp = fopen("temp.txt", "w"))
 					{
-						int tmpgn, tmpdur, tmpact, brtemp = 0;
-						while (fscanf(dat, "%d %d %s %d", &tmpact, &tmpgn, keytemp, &tmpdur))
+						int tmpgn, tmpdur, tmpact;
+						while (fscanf(dat, "%d %d %s %d", &tmpact, &tmpgn, keytemp, &tmpdur)!=EOF)
 						{
-							brtemp++;
-							if (br == brtemp)
-							{
+							if (strcmp(keytemp, key) == 0)
 								fprintf(temp, "%d %d %s %d\n", 1, gamenumber, filekey, duration);
-								fseek(dat, 1, SEEK_CUR);
-							}
 							else
 								fprintf(temp, "%d %d %s %d\n", tmpact, tmpgn, keytemp, tmpdur);
 						}
 					}
+					fclose(dat);
+					fclose(temp);
 					remove("keys.txt");
 					rename("temp.txt", "keys.txt");
-					rewind(temp);
-					fclose(temp);
 					return 1;
 				}
 			}
-		}
-		rewind(dat);
-		fclose(dat);
+		} 
 	}
-	return 0;//ako je ključ pogrešan tj. ne postoji vraća 0
+	return 0; 
 }
-int CheckKeyTimer(int gamenumber, struct tm *timer)
+int CheckKeyTimer(int gamenumber)
 {
 	FILE *dat;
 	if (dat = fopen("keys_timer.txt", "r"))
 	{
 		int nmb, duration;
-		char time[27];
+		char timex[27];
 		do
 		{
-			fscanf(dat,"%d %s %d", &nmb, time, &duration);
+			fscanf(dat, "%d %s %d", &nmb, timex, &duration);
 		} while (nmb != gamenumber);
 		if (duration == -1)
-			return 0;//vraca 0 ako igrica nije ni aktivirana(trajanje kljuca je zapisano kao -1)
+			return 0; 
 		else if (!duration)
-			return 1;//vraća 1 ako je trajanje ključa beskonačno(zapisano u fajlu kao 0)
+			return 1; 
 		else
 		{
-			struct tm currenttm;
-			localtime_s(&currenttm, &(time_t) { time(NULL) });
-			time_t temp = date_string_to_time(time);
+			time_t currenttm;  time(&currenttm); 
+ 			time_t temp = date_string_to_time(time);
 			double diff = difftime(currenttm, temp);
 			if (diff > duration)
-				return 0;//ako je isteklo trajanje ključa vraća 0
+				return 0; 
 			else
-				return 1;//ako nije isteklo vraća 1
+				return 1; 
 		}
 	}
 }
+ 
